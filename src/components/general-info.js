e97de09cd25e3ccdd10042bae9bf21fa8ef021ff
randy steward <rsteward117@gmail.com>
2021-05-19 00:27:22 -0500

    made changes to general-info.js

--- src/components/general-info.js
+++ src/components/general-info.js
@@ -1,13 +1,63 @@
-import React from 'react';
-
-    const GeneralInformation = (props) => {
-        const {name, number, email } = props; 
-    
-
-    return(
-        <p>
-            {props.name}
-        </p>
-    )
+import React, {Component} from 'react';
+class GeneralInformation extends Component{
+    constructor(){
+        super()
+        this.state={
+            name: '',
+            number: '',
+            email: ''
+        }
+    }
+    handleNameChange = (e) =>{
+       this.setState({
+           name: e.target.value
+       }); 
+    }
+    handleNumberChange = (e) =>{
+        this.setState({
+           number: e.target.value
+       }); 
+    }
+    handleEmailChange = (e) =>{
+        this.setState({
+           email: e.target.value
+       }); 
+    }
+    handleSubmit = (e) =>{
+        
+        let createParagraph = document.createElement('p');
+        createParagraph.textContent = `Name:${this.state.name}`
+        e.preventDefault();
+    } 
+    render(){
+        const {name, number, email} = this.state;
+        return(
+            <div>
+                <h1>Personal Information </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="Name">Name: </label>
+                    <input
+                    type="text"
+                    value={name}
+                    onChange={this.handleNameChange}
+                        />
+                    <label htmlFor="Number">Phone Number: </label>
+                    <input
+                    type="tel"
+                    pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+                    value={number}
+                    onChange={this.handleNumberChange}
+                        />
+                    <label htmlFor="Email">Email Address: </label>
+                    <input
+                    type="email"
+                    value={email}
+                    onChange={this.handleEmailChange}
+                        />
+                    <button type="submit" onClick>Submit </button>
+                </form>
+            </div>
+            )
+    }
 }
 export default GeneralInformation;
